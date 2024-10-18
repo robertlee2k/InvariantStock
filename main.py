e2449d87b0fe208bac667cf5e9a379bf1632daa5
robertlee2k <torobert@qq.com>
2024-10-18 18:47:20 +0800

    X1

--- main.py
+++ main.py
@@ -6,39 +6,39 @@ import numpy as np
 import os
 from tqdm.auto import tqdm
 import argparse
-from Layers import FeatureReconstructor, Predictor,FeatureMask, FeatureExtractor, FactorDecoder, FactorEncoder, FatorPrior, AlphaLayer, BetaLayer
-from dataset import StockDataset,DynamicBatchSampler
+from Layers import FeatureReconstructor, Predictor, FeatureMask, FeatureExtractor, FactorDecoder, FactorEncoder, \
+    FatorPrior, AlphaLayer, BetaLayer
+from dataset import StockDataset, DynamicBatchSampler
 from train_model import train, validate
-from utils import set_seed, DataArgument,generate_prediction_scores
+from utils import set_seed, DataArgument, generate_prediction_scores
 import wandb
 
 
 def rankic(df):
     ic = df.groupby('datetime').apply(lambda df: df["label"].corr(df["pred"]))
     ric = df.groupby('datetime').apply(lambda df: df["label"].corr(df["pred"], method="spearman"))
-    
+
     print({
-            "IC": ic.mean(),
-            "ICIR": ic.mean() / ic.std(),
-            "Rank IC": ric.mean(),
-            "Rank ICIR": ric.mean() / ric.std(),
-        })
+        "IC": ic.mean(),
+        "ICIR": ic.mean() / ic.std(),
+        "Rank IC": ric.mean(),
+        "Rank ICIR": ric.mean() / ric.std(),
+    })
     return ric
 
 
-
-
-
-def add_env(date,df):
-    month = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec",]
+def add_env(date, df):
+    month = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec", ]
     begin_year = 2020
     end_year = 2024
-    year = [str(i) for i in range(begin_year,end_year+1)]
-    df[month]=0
-    df[year]=0
-    df.loc[df.index.get_level_values("datetime") == date,month[date.month-1]]=1
-    df.loc[df.index.get_level_values("datetime") == date,year[date.year-begin_year]]=1
+    year = [str(i) for i in range(begin_year, end_year + 1)]
+    df[month] = 0
+    df[year] = 0
+    df.loc[df.index.get_level_values("datetime") == date, month[date.month - 1]] = 1
+    df.loc[df.index.get_level_values("datetime") == date, year[date.year - begin_year]] = 1
     return df
+
+
 def multi_add_env(dataset):
     pool = multiprocessing.Pool()
     results = pool.starmap(add_env, [*dataset.groupby("datetime")])
@@ -49,48 +49,52 @@ def multi_add_env(dataset):
 
 
 def main(args):
-
     set_seed(args.seed)
     if not os.path.exists(args.save_dir):
         os.makedirs(args.save_dir)
 
     # create model
-    #feature selection module
+    # feature selection module
     feature_reconstructor = FeatureReconstructor(feat_dim=args.feat_dim)
     feature_mask = FeatureMask(feat_dim=args.feat_dim, hidden_dim=args.feat_dim)
-    
-    #environment-agnostic module
+
+    # environment-agnostic module
     feature_extractor = FeatureExtractor(feat_dim=args.feat_dim, hidden_dim=args.hidden_dim)
     factor_encoder = FactorEncoder(factor_dims=args.factor_dim, num_portfolio=args.feat_dim, hidden_dim=args.hidden_dim)
     alpha_layer = AlphaLayer(args.hidden_dim)
     beta_layer = BetaLayer(args.hidden_dim, args.factor_dim)
     factor_decoder = FactorDecoder(alpha_layer, beta_layer)
     factor_prior_model = FatorPrior(args.batch_size, args.hidden_dim, args.factor_dim)
-    predictor = Predictor(feature_extractor, factor_encoder, factor_decoder, factor_prior_model,args)
+    predictor = Predictor(feature_extractor, factor_encoder, factor_decoder, factor_prior_model, args)
 
-    #environment-awareness module
-    env_feature_extractor = FeatureExtractor(feat_dim=args.feat_dim+args.env_size, hidden_dim=args.hidden_dim)
-    env_factor_encoder = FactorEncoder(factor_dims=args.factor_dim, num_portfolio=args.feat_dim, hidden_dim=args.hidden_dim)
+    # environment-awareness module
+    env_feature_extractor = FeatureExtractor(feat_dim=args.feat_dim + args.env_size, hidden_dim=args.hidden_dim)
+    env_factor_encoder = FactorEncoder(factor_dims=args.factor_dim, num_portfolio=args.feat_dim,
+                                       hidden_dim=args.hidden_dim)
     env_alpha_layer = AlphaLayer(args.hidden_dim)
     env_beta_layer = BetaLayer(args.hidden_dim, args.factor_dim)
     env_factor_decoder = FactorDecoder(env_alpha_layer, env_beta_layer)
     env_factor_prior_model = FatorPrior(args.batch_size, args.hidden_dim, args.factor_dim)
-    env_predictor = Predictor(env_feature_extractor, env_factor_encoder, env_factor_decoder, env_factor_prior_model,args)
+    env_predictor = Predictor(env_feature_extractor, env_factor_encoder, env_factor_decoder, env_factor_prior_model,
+                              args)
 
     # create dataloaders
     # Dynamic batch size 
-    train_ds = StockDataset(dataset,train_index)
-    valid_ds = StockDataset(dataset,valid_index)
-    test_ds = StockDataset(dataset,valid_index)
+    train_ds = StockDataset(dataset, train_index)
+    valid_ds = StockDataset(dataset, valid_index)
+    test_ds = StockDataset(dataset, valid_index)
 
-    train_batch_sizes = pd.DataFrame([ i[0] for i in dataset.index[train_index[:,0]].values]).value_counts(sort=False).values
-    train_batch_sampler = DynamicBatchSampler(train_ds,train_batch_sizes)
- 
-    valid_batch_sizes = pd.DataFrame([ i[0] for i in dataset.index[valid_index[:,0]].values]).value_counts(sort=False).values
-    valid_batch_sampler = DynamicBatchSampler(valid_ds,valid_batch_sizes)
-    
-    test_batch_sizes = pd.DataFrame([ i[0] for i in dataset.index[test_index[:,0]].values]).value_counts(sort=False).values
-    test_batch_sampler = DynamicBatchSampler(test_ds,test_batch_sizes)
+    train_batch_sizes = pd.DataFrame([i[0] for i in dataset.index[train_index[:, 0]].values]).value_counts(
+        sort=False).values
+    train_batch_sampler = DynamicBatchSampler(train_ds, train_batch_sizes)
+
+    valid_batch_sizes = pd.DataFrame([i[0] for i in dataset.index[valid_index[:, 0]].values]).value_counts(
+        sort=False).values
+    valid_batch_sampler = DynamicBatchSampler(valid_ds, valid_batch_sizes)
+
+    test_batch_sizes = pd.DataFrame([i[0] for i in dataset.index[test_index[:, 0]].values]).value_counts(
+        sort=False).values
+    test_batch_sampler = DynamicBatchSampler(test_ds, test_batch_sizes)
 
     train_dataloader = DataLoader(train_ds, batch_sampler=train_batch_sampler, shuffle=False, num_workers=4)
     valid_dataloader = DataLoader(valid_ds, batch_sampler=valid_batch_sampler, shuffle=False, num_workers=4)
@@ -100,37 +104,60 @@ def main(args):
     predictor.to(device)
     best_rankic = 0
 
-    featrue_optimizer = torch.optim.Adam(list(feature_reconstructor.parameters())+list(feature_mask.parameters()), lr=args.lr)
-    featrue_scheduler = torch.optim.lr_scheduler.OneCycleLR(featrue_optimizer,pct_start=0.1, max_lr=args.lr, steps_per_epoch=len(train_dataloader), epochs=args.num_epochs//3)
+    featrue_optimizer = torch.optim.Adam(list(feature_reconstructor.parameters()) + list(feature_mask.parameters()),
+                                         lr=args.lr)
+    featrue_scheduler = torch.optim.lr_scheduler.OneCycleLR(featrue_optimizer, pct_start=0.1, max_lr=args.lr,
+                                                            steps_per_epoch=len(train_dataloader),
+                                                            epochs=args.num_epochs // 3)
 
     optimizer = torch.optim.Adam(predictor.parameters(), lr=args.lr)
-    scheduler = torch.optim.lr_scheduler.OneCycleLR(optimizer, max_lr=args.lr,pct_start=0.1, steps_per_epoch=len(train_dataloader), epochs=args.num_epochs//3)
+    scheduler = torch.optim.lr_scheduler.OneCycleLR(optimizer, max_lr=args.lr, pct_start=0.1,
+                                                    steps_per_epoch=len(train_dataloader), epochs=args.num_epochs // 3)
 
     env_optimizer = torch.optim.Adam(env_predictor.parameters(), lr=args.lr)
-    env_scheduler = torch.optim.lr_scheduler.OneCycleLR(env_optimizer, max_lr=args.lr,pct_start=0.1, steps_per_epoch=len(train_dataloader), epochs=args.num_epochs//3)
-
+    env_scheduler = torch.optim.lr_scheduler.OneCycleLR(env_optimizer, max_lr=args.lr, pct_start=0.1,
+                                                        steps_per_epoch=len(train_dataloader),
+                                                        epochs=args.num_epochs // 3)
 
     # Start Trainig
     for epoch in tqdm(range(args.num_epochs)):
-        train_loss,pred_loss,env_loss,env_pred_loss,diff_loss,self_pred_loss,recon_diff_loss,kl_diff_loss,rank_loss,env_rank_loss,kl_loss,env_kl_loss,rank_diff_loss = train(feature_reconstructor,feature_mask,predictor,env_predictor, train_dataloader, featrue_optimizer, optimizer,env_optimizer,featrue_scheduler,scheduler,env_scheduler,args,epoch=epoch)
-        val_loss,val_pred_loss,val_rank_loss,val_kl_loss,avg_rankic = validate(feature_mask,predictor, valid_dataloader,args)
+        train_loss, pred_loss, env_loss, env_pred_loss, diff_loss, self_pred_loss, recon_diff_loss, kl_diff_loss, rank_loss, env_rank_loss, kl_loss, env_kl_loss, rank_diff_loss = train(
+            feature_reconstructor, feature_mask, predictor, env_predictor, train_dataloader, featrue_optimizer,
+            optimizer, env_optimizer, featrue_scheduler, scheduler, env_scheduler, args, epoch=epoch)
+        val_loss, val_pred_loss, val_rank_loss, val_kl_loss, avg_rankic = validate(feature_mask, predictor,
+                                                                                   valid_dataloader, args)
         path = path = epoch % 3
-        print(f"Epoch {epoch+1}: ",{"Validation Toal Loss": round(val_loss,6),"Validation Pred Loss": round(val_pred_loss,6),"Validation Ranking Loss": round(val_rank_loss,6),"Validation KL Loss": round(val_kl_loss,6),"Validation RankIC":round(avg_rankic,6)})
+        print(f"Epoch {epoch + 1}: ",
+              {"Validation Toal Loss": round(val_loss, 6), "Validation Pred Loss": round(val_pred_loss, 6),
+               "Validation Ranking Loss": round(val_rank_loss, 6), "Validation KL Loss": round(val_kl_loss, 6),
+               "Validation RankIC": round(avg_rankic, 6)})
         if args.wandb:
-            wandb.log({"Validation Toal Loss": val_loss,"Validation Pred Loss": val_pred_loss,"Validation Ranking Loss": val_rank_loss,"Validation KL Loss": val_kl_loss,"Validation RankIC":avg_rankic}, step=epoch)
+            wandb.log({"Validation Toal Loss": val_loss, "Validation Pred Loss": val_pred_loss,
+                       "Validation Ranking Loss": val_rank_loss, "Validation KL Loss": val_kl_loss,
+                       "Validation RankIC": avg_rankic}, step=epoch)
             if path == 0:
-                wandb.log({"Different Loss": diff_loss,"Self Reconstruction Loss":self_pred_loss,"Reconstruction Diff Loss":recon_diff_loss,"KL Diff Loss":kl_diff_loss}, step=epoch)
+                wandb.log({"Different Loss": diff_loss, "Self Reconstruction Loss": self_pred_loss,
+                           "Reconstruction Diff Loss": recon_diff_loss, "KL Diff Loss": kl_diff_loss}, step=epoch)
             elif path in [1]:
-                wandb.log({"No Env Loss": train_loss,"No Env Pred Loss": pred_loss,"No Env Ranking Loss": rank_loss,"No Env KL Loss": kl_loss}, step=epoch)
+                wandb.log({"No Env Loss": train_loss, "No Env Pred Loss": pred_loss, "No Env Ranking Loss": rank_loss,
+                           "No Env KL Loss": kl_loss}, step=epoch)
             elif path in [2]:
-                wandb.log({"With Env Loss": env_loss,"With Env Pred Loss": env_pred_loss,"With Env Ranking Loss": env_rank_loss,"With Env KL Loss": env_kl_loss}, step=epoch)
+                wandb.log({"With Env Loss": env_loss, "With Env Pred Loss": env_pred_loss,
+                           "With Env Ranking Loss": env_rank_loss, "With Env KL Loss": env_kl_loss}, step=epoch)
 
         if path == 0:
-            print(f"Epoch {epoch+1}: ",{"Different Loss": round(diff_loss,6),"Self Reconstruction Loss":round(self_pred_loss,6),"Reconstruction Diff Loss":round(recon_diff_loss,6),"KL Diff Loss":round(kl_diff_loss,6),"Rank Diff Loss":round(rank_diff_loss,6)})
+            print(f"Epoch {epoch + 1}: ",
+                  {"Different Loss": round(diff_loss, 6), "Self Reconstruction Loss": round(self_pred_loss, 6),
+                   "Reconstruction Diff Loss": round(recon_diff_loss, 6), "KL Diff Loss": round(kl_diff_loss, 6),
+                   "Rank Diff Loss": round(rank_diff_loss, 6)})
         elif path in [1]:
-            print(f"Epoch {epoch+1}: ",{"No Env Loss": round(train_loss,6),"No Env Pred Loss": round(pred_loss,6),"No Env Ranking Loss": round(rank_loss,6),"No Env KL Loss": round(kl_loss,6)})
+            print(f"Epoch {epoch + 1}: ", {"No Env Loss": round(train_loss, 6), "No Env Pred Loss": round(pred_loss, 6),
+                                           "No Env Ranking Loss": round(rank_loss, 6),
+                                           "No Env KL Loss": round(kl_loss, 6)})
         elif path in [2]:
-            print(f"Epoch {epoch+1}: ",{"With Env Loss": round(env_loss,6),"With Env Pred Loss": round(env_pred_loss,6),"With Env Ranking Loss": round(env_rank_loss,6),"With Env KL Loss": round(env_kl_loss,6)})
+            print(f"Epoch {epoch + 1}: ",
+                  {"With Env Loss": round(env_loss, 6), "With Env Pred Loss": round(env_pred_loss, 6),
+                   "With Env Ranking Loss": round(env_rank_loss, 6), "With Env KL Loss": round(env_kl_loss, 6)})
 
         if avg_rankic > best_rankic:
             best_rankic = avg_rankic
@@ -138,19 +165,21 @@ def main(args):
             feat_mask_root = os.path.join(args.save_dir, f'best_feat_mask_{args.run_name}_{epoch}.pt')
             torch.save(predictor.state_dict(), predictor_root)
             torch.save(feature_mask.state_dict(), feat_mask_root)
-    
-    #loading the best model for the final test set
-    predictor.load_state_dict(torch.load(predictor_root))
-    feature_mask.load_state_dict(torch.load(feat_mask_root))
-    output = generate_prediction_scores(feature_mask,predictor,test_dataloader,args)
-    output.index = dataset.index[test_index[:,-1]]
-    output["label"] = dataset.loc[output.index,'label']
+
+        # loading the best model for the final test set
+    predictor.load_state_dict(torch.load(predictor_root, weights_only=True))
+    feature_mask.load_state_dict(torch.load(feat_mask_root, weights_only=True))
+
+    output = generate_prediction_scores(feature_mask, predictor, test_dataloader, args)
+    output.index = dataset.index[test_index[:, -1]]
+    output["label"] = dataset.loc[output.index, 'label']
     print("Test Result:")
     rankic(output)
-    
+
     if args.wandb:
         wandb.log({"Best Validation RankIC": best_rankic})
         wandb.finish()
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Train a predictor model on stock data')
